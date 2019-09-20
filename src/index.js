223c68b427aac6616c833f57d77f185a6af63c7f
sergx <serg_x@bk.ru>
2019-09-20 18:33:38 +0300

    add index files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import './js/common.js'
+import './css/main.css'
+import './scss/main.scss'
+//import './js/common' - Можно и без точки
+
+//import 'vue' // Такая запись актуальна для Модуля, т.к. Webpack найдет все в package.json
+//import Vue from 'vue' // Так тоже можно
+//import 'bootstrap/dist/css/bootstrap.css' // Можно и css импортировать
+
+window.Vue = require('vue'); // Вроже вот так пишут
