c5d64f755d3d79d03e287d6c3db74fd6065c5677
sergx <serg_x@bk.ru>
2019-09-24 00:55:22 +0300

    split to vendors.js and app.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -14,34 +14,47 @@ const PATHS = {
 
 //console.log(__dirname);
 //console.log(path.resolve(__dirname, 'path'));
-
+console.log(path.resolve(__dirname, "src/css"));
 module.exports = {
 	externals:{
 		paths: PATHS
 	},
 	entry:{
 		app: PATHS.src,
-		//ser: "./src/server.js" // Может быть несколько точек входа
+    //lk: `${PATHS.src}/lk.js`,  // Может быть несколько точек входа
+		//ser: "./src/server.js"
 	},
 	output:{ // точки выхода
-		filename: `${PATHS.assets}js/[name].js`,
+		filename: `${PATHS.assets}js/[name].js?v=[hash]`,
 		//filename: '[name].js', //  Квадратные скобки означают, что берется файл с имеем точки входа https://youtu.be/JcKRovPhGo8?t=916
 		path: PATHS.dist, // папка назначения скомпилированных файлов https://nodejs.org/api/path.html#path_path_relative_from_to
 		publicPath: '/' // Папка, которая отображается, может отличаться от реальной папки
 	},
+  optimization:{ // Разбиваем на отдельные файлы
+    splitChunks:{
+      cacheGroups:{
+        vendor:{
+          test: /node_modules/,
+          name: 'vendors',
+          chunks: 'all',
+          enforce: true,
+        }
+      }
+    }
+  },
 	module:{ // Подключаемые модули
 		rules:[{
 			test: /\.js$/,
 			loader: "babel-loader",
-			//exclude: "/node_modules/" // не включаем те файлы, которые содержет эта папка
-			exclude: path.resolve(__dirname, "node_modules"),
-			//include: [path.resolve(__dirname, "src")]
+			exclude: "/node_modules/", // не включаем те файлы, которые содержет эта папка
+			//exclude: path.resolve(__dirname, "node_modules"),
+			//include: [ /src\/js/ /*path.resolve(__dirname, "src/js")*/]
 		},{
 			test: /\.vue$/,
 			loader: "vue-loader",
       options:{
         loader:{
-          scss: 'vue-style-loader!css-loader!sass-loader'
+          scss: 'vue-style-loader!css-loader!sass-loader' // то, затем се, затем сё
         }
       }
 		},{
@@ -94,12 +107,13 @@ module.exports = {
 	plugins: [
     new VueLoaderPlugin(),
 		new MiniCssExtractPlugin({
-			filename: `${PATHS.assets}css/[name].css`,
+			filename: `${PATHS.assets}css/[name].[hash].css`,
 		}),
 		new HtmlWebpackPlugin({
-			hash: false,
 			template: `${PATHS.src}/index.html`,
-			filename: './index.html'
+			filename: './index.html',
+      title: 'Webpack template O',
+      inject: false,
 		}),
 		new CopyWebpackPlugin([
 			{ from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
