5932657ee8793f090b4a0801de318f664f8d0f48
sergx <serg_x@bk.ru>
2019-09-23 16:24:45 +0300

    add

--- webpack.build.conf.js
+++ webpack.build.conf.js
@@ -1,1 +1,9 @@
- --mode production
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
+const buildWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'production',
+  plugins: []
+});
+ module.export = new Promise((resolve, reject) => {
+   resolve(buildWebpackConfig);
+ });
